4991e701512a404ea6b8ea75af6a19d1bdf61c03
Mapstor <marko.visic7@gmail.com>
2026-06-30 22:02:18 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://ads.adthrive.com/sites/6a3d589918ca6927370d0a51/ads.txt',
+        statusCode: 301,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
